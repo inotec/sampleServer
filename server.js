d6e28d57c4fd50c6abd74fe5ae54e54724268c44
inotec <inotec@ieee.org>
2017-08-04 17:57:04 -0400

    Query Object

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
-
+/*
 app.get('/addViaParams/:numberOne/:numberTwo', function (req, res) {
 	// here we can get the two numbers off the params object
 	// Converth them to numbers because they come in as strings
@@ -14,6 +14,13 @@ app.get('/addViaParams/:numberOne/:numberTwo', function (req, res) {
 											// notice those are backtick marks not signle quotes
 
 });
+*/
+app.get('/addViaQuery', function (req, res) {
+	var numberOne = Number(req.query.numberOne);
+	var numberTwo = Number(req.query.numberTwo);
+
+	res.send(`${numberOne + numberTwo}`);  // convert back to string
+})
 
 app.listen(port, function () {
 	console.log('Example app listening on port: ' + port);
