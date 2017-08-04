4678bb03440e13cec614c68426b7f40d878f6f6a
inotec <inotec@ieee.org>
2017-08-04 17:00:09 -0400

    add addViaParams endpoint

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.send('hello world');
+app.get('/addViaParams/:numberOne/:numberTwo', function (req, res) {
+	// here we can get the two numbers off the params object
+	// Converth them to numbers because they come in as strings
+	var numberOne = Number(req.params.numberOne);
+	var numberTwo = Number(req.params.numberTwo);
+
+	// Then we can respond with the total
+	res.send(`${numberOne + numberTwo}`);   // convert back to string
+											// notice those are backtick marks not signle quotes
+
 });
 
 app.listen(port);
