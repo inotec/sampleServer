bf7ecba9a1805c309826d36525753c6ce8574cd2
inotec <inotec@ieee.org>
2017-08-04 16:45:02 -0400

    fixed port and script for heroku

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.listen(3000);
+app.listen(port);
