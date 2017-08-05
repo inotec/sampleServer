d80c06bf8e2bbadee2f009a2e709aa4183666bd2
inotec <inotec@ieee.org>
2017-08-05 16:16:32 -0400

    Get Contact

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const morgan = require('morgan');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
 var contacts = [];
-var contactID = 1;
+var contactID = 100;
 
 /*
 app.use(function (req, res, next) {
@@ -66,6 +67,16 @@ app.post('/contacts', function (req, res) {
   res.status(201).send(contactInfo);
 });
 
+app.get('/contacts/:id', function (req, res) {
+  for (var i=0; i<contacts.length; i++) {
+    if (contacts[i].id == Number(req.params.id)) {
+      return res.status(200).send(contacts[i]);
+    }
+  }
+  return res.status(404).send({ message: 'contact not found' });
+});
+	
+//
 
 app.listen(port, function () {
 	console.log('Example app listening on port: ' + port);
