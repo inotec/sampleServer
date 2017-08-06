a9792eb7b8b9281901838fcd65e347d22bd8d40d
inotec <inotec@ieee.org>
2017-08-06 11:02:16 -0400

    Delete

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ var bodyParser = require('body-parser');
 var contacts = [];
 var contactID = 1;
 
-/*
-app.use(function (req, res, next) {
-  console.log(`request made to ${req.method} ${req.originalUrl}`);
-  next();
-});
-*/
+
 app.use(morgan('dev'));
 
 app.use(bodyParser.json());
@@ -21,36 +16,9 @@ app.use(bodyParser.json());
 
 
 var port = process.env.PORT || 3000;
-/*
-app.get('/addViaParams/:numberOne/:numberTwo', function (req, res) {
-	// here we can get the two numbers off the params object
-	// Converth them to numbers because they come in as strings
-	var numberOne = Number(req.params.numberOne);
-	var numberTwo = Number(req.params.numberTwo);
 
-	// Then we can respond with the total
-	res.send(`${numberOne + numberTwo}`);   // convert back to string
-											// notice those are backtick marks not signle quotes
 
-});
-*/
-
-/*
-app.get('/addViaQuery', function (req, res) {
-	var numberOne = Number(req.query.numberOne);
-	var numberTwo = Number(req.query.numberTwo);
-
-	res.send(`${numberOne + numberTwo}`);  // convert back to string
-})
-*/
-
-/*
-app.post('/contacts', function (req, res) {
-	console.log(req);
-	res.send('ok');
-});
-*/
-
+// GET with localhost:3000/contacts/2 in browser URL will get info on id=2
 app.get('/contacts/:id', function (req, res) {
   for (var i=0;i<contacts.length;i++) {
     if (contacts[i].id == Number(req.params.id)) {
@@ -60,6 +28,7 @@ app.get('/contacts/:id', function (req, res) {
   return res.status(404).send({ message: 'contact not found' });
 });
 
+// POST with localhost:3000/contacts will store info in next array element
 app.post('/contacts', function (req, res) {
   // grab the posted info
   var contactInfo = req.body;
@@ -79,11 +48,12 @@ app.post('/contacts', function (req, res) {
   res.status(201).send(contactInfo);
 });
 
-
+// PUT with localhost:3000/contacts/2 will replace info for element with id=2
 app.put('/contacts/:id', function (req, res) {
   for (var i=0;i<contacts.length;i++) {
     if (contacts[i].id == Number(req.params.id)) {
       contacts[i] = Object.assign(contacts[i], req.body);
+      console.log(req.body);
       console.log(contacts[i]);
       return res.status(200).send(contacts[i]);
     }
@@ -91,6 +61,16 @@ app.put('/contacts/:id', function (req, res) {
   return res.status(404).send({ message: 'contact not found'});
 });
 
+app.delete('/contacts/:id', function (req, res) {
+  for (var i=0;i<contacts.length;i++) {
+    if (contacts[i].id == Number(req.params.id)) {
+      contacts.splice(i,1);
+      console.log(contacts);
+      return res.status(200).send({ message: 'contact deleted' });
+    }
+  }
+  return res.status(404).send({ message: 'contact not found' });
+});
 	
 app.listen(port, function () {
 	console.log('Example app listening on port: ' + port);
