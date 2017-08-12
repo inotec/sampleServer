984ccb5ce8eb3437448885758a7c901eff4300dc
inotec <inotec@ieee.org>
2017-08-12 11:45:13 -0400

    Empty search object

--- contact.spec.js
+++ contact.spec.js
@@ -12,76 +12,73 @@ describe('A Contact', function () {
   });
  });
 
-describe('find', function () {
-  	it.only ('finds by name', function (done) {
-     Contact.contacts = [
-                           {
-                             name: 'bob',
-                             email: 'bob@bob.com',
-                             id: 1
-                           },
-                           {
-                             name: 'sam',
-                             email: 'sam@sam.com',
-                             id: 2
-                           },
-                           {
-                             name: 'bill',
-                             email: 'bill@bill.com',
-                             id: 3
-                           },
-                           {
-                             name: 'bob',
-                             email: 'bob@bob.com',
-                             id: 4
-                           },
-                           {
-                             name: 'bill',
-                             email: 'bill@bill.com',
-                             id: 5
-                           }
-                         ];
-     Contact.contactsID = 6;
+  describe.only('find', function () {
+    beforeEach(function () {
+      Contact.contacts = [
+                            {
+                              name: 'bob',
+                              email: 'bob@bob.com',
+                              id: 1
+                            },
+                            {
+                              name: 'sam',
+                              email: 'sam@sam.com',
+                              id: 2
+                            },
+                            {
+                              name: 'bill',
+                              email: 'bill@bill.com',
+                              id: 3
+                            },
+                            {
+                              name: 'bob',
+                              email: 'bob@bob.com',
+                              id: 4
+                            },
+                            {
+                              name: 'bill',
+                              email: 'bill@bill.com',
+                              id: 5
+                            }
+                          ];
+      Contact.contactsID = 6;
+    });
+    
+    it.only ('finds by name', function (done) {
+      Contact.find({ name: 'bob' }, function (err, foundContacts) {
+        expect(foundContacts.length).to.equal(2);
+        var ids = foundContacts.map((contact) => contact.id);
+        expect(ids).to.contain(1);
+        expect(ids).to.contain(4);
 
-     Contact.find({ name: 'bob' }, function (err, foundContacts) {
-       expect(foundContacts.length).to.equal(2);
-       var ids = foundContacts.map((contact) => contact.id);
-       expect(ids).to.contain(1);
-       expect(ids).to.contain(4);
+        Contact.find({ name: 'sam' }, function (err, otherFoundContacts) {
+          expect(otherFoundContacts.length).to.equal(1);
+          expect(otherFoundContacts[0].id).to.equal(2);
+          done();
+        });
+      });
+    });
 
-       Contact.find({ name: 'sam' }, function (err, otherFoundContacts) {
-         expect(otherFoundContacts.length).to.equal(1);
-         expect(otherFoundContacts[0].id).to.equal(2);
-         done();
+	it.only ('finds by email', function (done) {
+      Contact.find({ email: 'bob@bob.com' }, function (err, foundContacts) {
+        expect(foundContacts.length).to.equal(2);
+        var ids = foundContacts.map((contact) => contact.id);
+        expect(ids).to.contain(1);
+        expect(ids).to.contain(4);
 
-		  this.find = function (attrs, callback) {
-		    var nameToMatch = attrs.name;
-		    var emailToMatch = attrs.email;
-		    var results = [];
+        Contact.find({ email: 'sam@sam.com' }, function (err, otherFoundContacts) {
+          expect(otherFoundContacts.length).to.equal(1);
+          expect(otherFoundContacts[0].id).to.equal(2);
+          done();
+        });
+      });
+    });
 
-		    for (var i=0;i<self.contacts.length;i++) {
-		      var matchesName = true;
-		      var matchesEmail = true;
+    it('returns all contacts if search object is empty', function (done) {
+      Contact.find({}, function (err, foundContacts) {
+        expect(foundContacts.length).to.equal(5);
+        done();
+      });
+    });
 
-		      // IF a name was provided and it doesn't match...
-		      if (nameToMatch && (self.contacts[i].name != nameToMatch)) {
-		        matchesName = false;
-		      }
-
-		      // IF an email was provided and it doesn't match
-		      if (emailToMatch && (self.contacts[i].email != emailToMatch)) {
-		        matchesEmail = false;
-		      }
-
-		      // IF it passed both tests, include it in the results
-		      if (matchesName && matchesEmail) {
-		        results.push(self.contacts[i]);
-		      }
-		    }
-		    callback(null, results);
-		  };
-         
-       });
-     });
-   });
-});
+  });
